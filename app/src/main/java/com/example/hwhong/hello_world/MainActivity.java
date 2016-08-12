815de929a7af41d14e8c2ffef6150bccf0938a7a
warrenh <hwhong@ucsd.edu>
2016-08-12 13:48:56 +0800

    1st update

--- app/src/main/java/com/example/hwhong/hello_world/MainActivity.java
+++ app/src/main/java/com/example/hwhong/hello_world/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                textView.setText("Hello, Wolrd");
+                textView.setText("Hello, World");
             }
         });
 
