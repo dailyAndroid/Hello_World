5e29ee27f0e91816e296c55c01053c84daca13d4
warren <hwhong1129@gmail.com>
2016-08-12 13:05:06 +0800

    Initial commit

---
+++ app/src/main/java/com/example/hwhong/hello_world/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.hwhong.hello_world;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Hello, Wolrd");
+            }
+        });
+
+    }
+}
